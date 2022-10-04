716ecf72c6bc9fb45f26e8e9505b21592bf8d1e0
rafael dias <dias416@gmail.com>
2022-10-04 21:12:44 +0100

    refactoring to syring

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,45 +1,35 @@
-import { Category } from "../../model/Category";
+import { getRepository, Repository } from "typeorm";
+
+import { Category } from "../../entities/Category";
 import {
     ICategoriesRepository,
     ICreateCategoryDTO,
 } from "../ICategoriesRepository";
 
 class CategoriesRepository implements ICategoriesRepository {
-    private categories: Category[];
+    private repository: Repository<Category>;
 
-    // eslint-disable-next-line no-use-before-define
-    private static INSTANCE: CategoriesRepository;
-
-    private constructor() {
-        this.categories = [];
+    constructor() {
+        this.repository = getRepository(Category);
     }
 
-    public static getInstance(): CategoriesRepository {
-        if (!CategoriesRepository.INSTANCE) {
-            CategoriesRepository.INSTANCE = new CategoriesRepository();
-        }
-        return CategoriesRepository.INSTANCE;
-    }
-    create({ description, name }: ICreateCategoryDTO): void {
-        const category = new Category();
-
-        Object.assign(category, {
-            name,
+    async create({ description, name }: ICreateCategoryDTO): Promise<void> {
+        const category = this.repository.create({
             description,
-            created_at: new Date(),
+            name,
         });
 
-        this.categories.push(category);
-    }
-    list(): Category[] {
-        return this.categories;
+        await this.repository.save(category);
     }
 
-    findByName(name: string): Category {
-        const category = this.categories.find(
-            (category) => category.name === name
-        );
+    async list(): Promise<Category[]> {
+        const categories = await this.repository.find();
+        return categories;
+    }
 
+    async findByName(name: string): Promise<Category> {
+        // select * from categories where name = "name" limit 1
+        const category = await this.repository.findOne({ name });
         return category;
     }
 }
--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -1,13 +1,18 @@
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+/* import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import { CreateCategoryController } from "./CreateCategoryController";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-const categoriesRepository = CategoriesRepository.getInstance();
+export default (): CreateCategoryController => {
+    const categoriesRepository = new CategoriesRepository();
 
-const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+    const createCategoryUseCase = new CreateCategoryUseCase(
+        categoriesRepository
+    );
 
-const createCategoryController = new CreateCategoryController(
-    createCategoryUseCase
-);
+    const createCategoryController = new CreateCategoryController(
+        createCategoryUseCase
+    );
 
-export { createCategoryController };
+    return createCategoryController;
+};
+ */
