ac703e8d71c22ffd75d7aab918bbe6d861c3ac6e
rafael dias <dias416@gmail.com>
2023-02-18 14:28:04 +0000

    avatar

---
+++ src/@types/express/index.ts
@@ -0,0 +1,10 @@
+// sobrescrever tipagem de request no express
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare namespace Express {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    export interface Request {
+        user: {
+            id: string;
+        };
+    }
+}
